697ac15a887f33a04ec0440d5fa69665c882375a
brandonhach <specialcambo@gmail.com>
2024-03-22 11:37:44 -0400

    error handling

--- models/cheese.js
+++ models/cheese.js
@@ -6,7 +6,12 @@ const cheeseSchema = new Schema(
 		title: { type: String, required: [true, 'Title is required'] },
 		seller: { type: String, required: [true, 'Seller is required'] },
 		condition: { type: String, required: [true, 'Condition is required'] },
-		price: { type: Number, required: [true, 'Price is required'] },
+		price: {
+			type: Number,
+			required: [true, 'Price is required'],
+			min: [10, 'minimum is $10'],
+			max: [10000, 'maximum is $10000'],
+		},
 		details: { type: String, required: [true, 'Details are required'], maxlength: 250 },
 		image: { type: String, required: [true, 'Image is required'] },
 		totalOffers: { type: Number, required: [true, 'Total offers is required'], default: 0 },
